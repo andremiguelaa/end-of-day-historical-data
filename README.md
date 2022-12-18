19778a0b4c36df5372743e8704c0756aa5581421
André Ascensão <andre.ascensao@bitsighttech.com>
2022-12-18 03:20:00 +0000

    wip

--- README.md
+++ README.md
@@ -1,28 +1,18 @@
 # End-of-Day Historical Data
 
-A simple Node.js script to get End of Day Historial Data for cryptos and stocks
+A simple Node.js script to get End of Day Historical Data for cryptos and stocks
 
 ## Setup
 
 1. `yarn`
-2. `cp .env.example .env`
-3. Add your CryptoCompare API Key to `.env` (CRYPTOCOMPARE_API_KEY)
 
 ## Usage
 
-`yarn get --currency=EUR --type=crypto --ticker=BTC [--filename=bitcoin]`
+`yarn get --symbol=AAPL --exchange=NASDAQ [--filename=my_favorite_stock]`
 
-or
-
-`yarn get --currency=EUR --type=etf --isin=IE00B5BMR087 [--filename=my_favorite_etf]`
-
-- currency: EUR, USD, CHF, GBP, etc.
-- type: stock, crypto, etf
-- ticker: VTI, SXR8.DE, BTC, etc.
-- isin: IE00B5BMR087, IE00B4L5Y983, etc.
+- symbol: AAPL, SXR8, etc.
+- exchange: Xetra, Frankfurt, NASDAQ,  etc.
 
 ## Data source
 
-- Stocks: https://finance.yahoo.com/
-- Cryptocurrencies: https://min-api.cryptocompare.com/
-- ETFs: https://www.justetf.com/
+- https://www.investing.com/
--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const colors = require("colors");
 const axios = require("axios");
 const moment = require("moment");
 const puppeteer = require("puppeteer");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-const TICKER_TYPES = ["stock", "crypto", "etf"];
-const ETF_CURRENCIES = ["EUR", "USD", "CHF", "GBP"];
 const log = console.log;
 const argv = yargs(process.argv).argv;
 
@@ -33,122 +33,68 @@ const saveFile = (historicalData) => {
 
 log(colors.yellow.bgBlack.underline("\nEnd of Day Historical Data\n"));
 
-if (
-  !TICKER_TYPES.includes(argv.type) ||
-  (argv.type === "etf" && !argv.isin) ||
-  (argv.type === "stock" && !argv.ticker) ||
-  (argv.type === "crypto" && !argv.ticker) ||
-  !argv.currency
-) {
+if (!argv.symbol || !argv.exchange) {
   log(colors.red("Invalid arguments! 😖\n"));
   return;
 }
 
-if (argv.type === "crypto") {
-  axios
-    .get(
-      `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${argv.ticker}&tsym=${argv.currency}&allData=true&api_key=${process.env.CRYPTOCOMPARE_API_KEY}`
-    )
-    .then((response) => {
-      if (response.data.Response === "Error") {
-        log(colors.red(`${response.data.Message} 😖\n`));
-        return;
-      }
-      const historicalData = response.data.Data.Data.reduce((acc, day) => {
-        const date = moment.unix(day.time).format("YYYY-MM-DD");
-        acc[date] = day.close;
-        return acc;
-      }, {});
-      saveFile(historicalData);
-    })
-    .catch((err) => {
-      log(colors.red(err));
-    });
-}
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  );
+  await page.goto(`https://www.investing.com/`);
+  await page.focus(".topBar .topBarSearch.topBarInputSelected input");
+  await page.keyboard.type(argv.symbol);
 
-if (argv.type === "stock") {
-  axios
-    .get(
-      `https://query1.finance.yahoo.com/v8/finance/chart/${argv.ticker}?interval=1d&range=100y`
-    )
-    .then((response) => {
-      if (response.data.chart.result[0].meta.currency !== argv.currency) {
-        log(colors.red(`Quotes not available in this currency 😖\n`));
-        return;
-      }
-      const historicalData =
-        response.data.chart.result[0].indicators.quote[0].close.reduce(
-          (acc, value, index) => {
-            const date = moment
-              .unix(response.data.chart.result[0].timestamp[index])
-              .format("YYYY-MM-DD");
-            acc[date] = value;
-            return acc;
-          },
-          {}
+  page.on("response", async (response) => {
+    if (
+      response
+        .url()
+        .indexOf("https://www.investing.com/search/service/searchTopBar") !== -1
+    ) {
+      const responseJson = await response.json();
+      if (responseJson?.total?.quotes) {
+        const exchange = responseJson.quotes.find(
+          (quote) => quote.exchange === argv.exchange
         );
-      saveFile(historicalData);
-    })
-    .catch((err) => {
-      log(colors.red(err));
-    });
-}
-
-if (argv.type === "etf") {
-  if (!ETF_CURRENCIES.includes(argv.currency)) {
-    log(colors.red(`Quotes not available in this currency 😖\n`));
-    return;
-  }
-  const currencyValue = ETF_CURRENCIES.indexOf(argv.currency);
-  (async () => {
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    await page.goto(
-      `https://www.justetf.com/en/etf-profile.html?0&isin=${argv.isin}#chart`
-    );
-    await page.waitForSelector("#CybotCookiebotDialog");
-    await page.evaluate((sel) => {
-      document.querySelector(sel).remove();
-    }, "#CybotCookiebotDialogBodyUnderlay");
-    await page.evaluate((sel) => {
-      document.querySelector(sel).remove();
-    }, "#CybotCookiebotDialog");
-
-    page.on("response", async (response) => {
-      if (
-        response
-          .url()
-          .indexOf("https://www.justetf.com/style/daterangepicker.min.css") !==
-        -1
-      ) {
-        await page.waitForTimeout(2000);
-        let isValid;
-        await page
-          .click('[title="since inception"]')
-          .then(() => {
-            isValid = true;
-          })
-          .catch(() => {
-            isValid = false;
-          });
-        if (isValid) {
-          await page.select(
-            '[name="chartPanel:chart:content:optionsPanel:selectContainer:valueType"]',
-            "market_value"
+        if (exchange) {
+          await page.goto(
+            `https://www.investing.com${exchange.link}`.replace(
+              "?",
+              "-historical-data?"
+            )
           );
-          await page.select(
-            '[name="chartPanel:chart:content:optionsPanel:selectContainer:currencies"]',
-            currencyValue.toString()
-          );
+          await page.click("#onetrust-accept-btn-handler");
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          await page.click("#flatDatePickerCanvasHol");
+          await new Promise((resolve) => setTimeout(resolve, 500));
+          for (let index = 0; index < 10; index++) {
+            await page.keyboard.press("Backspace");
+          }
+          await page.keyboard.type("01/01/1970");
           page.on("response", async (response) => {
             if (
               response
                 .url()
                 .indexOf(
-                  "https://www.justetf.com/en/etf-profile.html?0-1.0-chartPanel-chart-content-optionsPanel-selectContainer-currencies&isin="
+                  "https://www.investing.com/instruments/HistoricalDataAjax"
                 ) !== -1
             ) {
               const responseText = await response.text();
+              console.log(responseText);
+              await browser.close();
+              /*
+              console.log('here');
+              const responseText = await response.text();
+              console.log(responseText);
+              /*
+              const parser = new DOMParser();
+              const htmlDoc = parser.parseFromString(responseText, "text/html");
+              console.log(htmlDoc);
+              */
+              /*
               const data = eval(
                 responseText.split("setData(")[1].split(", false")[0]
               );
@@ -159,13 +105,194 @@ if (argv.type === "etf") {
               }, {});
               saveFile(historicalData);
               await browser.close();
+              */
             }
           });
+          await page.waitForSelector("#applyBtn").then(() => page.click("#applyBtn"));
         } else {
-          log(colors.red(`Invalid ISIN 😖\n`));
+          log(colors.red("Unavailable exchange! 😖\n"));
           await browser.close();
         }
+      } else {
+        log(colors.red("Unavailable symbol! 😖\n"));
+        await browser.close();
+      }
+
+      /*
+      console.log('here');
+      const responseText = await response.text();
+      console.log(responseText);
+      /*
+      const parser = new DOMParser();
+      const htmlDoc = parser.parseFromString(responseText, "text/html");
+      console.log(htmlDoc);
+      */
+      /*
+      const data = eval(
+        responseText.split("setData(")[1].split(", false")[0]
+      );
+      const historicalData = data.reduce((acc, day) => {
+        const date = moment.unix(day[0] / 1000).format("YYYY-MM-DD");
+        acc[date] = day[1];
+        return acc;
+      }, {});
+      saveFile(historicalData);
+      await browser.close();
+      */
+    }
+  });
+
+  return;
+  await page.goto(`https://www.investing.com/search/?q=${argv.symbol}`);
+  await page.waitForNetworkIdle();
+  const body = await page.evaluate(() => document.documentElement.outerHTML);
+  const dom = new JSDOM(body);
+  const document = dom.window.document;
+  const exchanges = document.querySelectorAll(
+    ".js-inner-all-results-quotes-wrapper .fourth"
+  );
+  const exchange = Array.from(exchanges).find((exchange) =>
+    exchange.innerHTML.includes(argv.exchange)
+  );
+  if (exchange) {
+    await page.goto(
+      `https://www.investing.com${exchange.parentElement.getAttribute(
+        "href"
+      )}`.replace("?", "-historical-data?")
+    );
+    await page.click("#onetrust-accept-btn-handler");
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    await page.click("#flatDatePickerCanvasHol");
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    for (let index = 0; index < 10; index++) {
+      await page.keyboard.press("Backspace");
+    }
+    await page.keyboard.type("01/01/1970");
+
+    page.on("response", async (response) => {
+      if (
+        response
+          .url()
+          .indexOf(
+            "https://www.investing.com/instruments/HistoricalDataAjax"
+          ) !== -1
+      ) {
+        const responseText = await response.text();
+        console.log(responseText);
+        /*
+        console.log('here');
+        const responseText = await response.text();
+        console.log(responseText);
+        /*
+        const parser = new DOMParser();
+        const htmlDoc = parser.parseFromString(responseText, "text/html");
+        console.log(htmlDoc);
+        */
+        /*
+        const data = eval(
+          responseText.split("setData(")[1].split(", false")[0]
+        );
+        const historicalData = data.reduce((acc, day) => {
+          const date = moment.unix(day[0] / 1000).format("YYYY-MM-DD");
+          acc[date] = day[1];
+          return acc;
+        }, {});
+        saveFile(historicalData);
+        await browser.close();
+        */
       }
     });
-  })();
-}
+
+    await page.waitForSelector("#applyBtn").then(() => page.click("#applyBtn"));
+  } else {
+    log(colors.red("Unavailable symbol! 😖\n"));
+    await browser.close();
+  }
+  // await browser.close();
+  return;
+
+  page.on("response", async (response) => {
+    console.log(response.url());
+    if (
+      response
+        .url()
+        .indexOf("https://www.investing.com/search/service/searchTopBar") !== -1
+    ) {
+      let responseJson;
+      try {
+        responseJson = await response.json();
+      } catch {
+        console.log("here");
+        await page.keyboard.type("sdgsdg");
+      }
+      if (!responseJson) {
+        return;
+      }
+      let quote;
+      responseJson?.quotes.forEach((item) => {
+        if (item.symbol === argv.symbol && item.exchange === argv.exchange) {
+          quote = item;
+        }
+      });
+      if (quote) {
+        /*
+        await page.goto(
+          `https://www.investing.com/${quote.link}`.replace(
+            "?",
+            "-historical-data?",
+            { waitUntil: "networkidle2" }
+          )
+        );
+        await page.click("#onetrust-accept-btn-handler");
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        await page.click("#flatDatePickerCanvasHol");
+        for (let index = 0; index < 10; index++) {
+          await page.keyboard.press("Backspace");
+        }
+        await page.keyboard.type("01/01/1970");
+
+        /*
+        page.on("response", async (response) => {
+          // console.log(response);
+          if (
+            response
+              .url()
+              .indexOf(
+                "https://www.investing.com/instruments/HistoricalDataAjax"
+              ) !== -1
+          ) {
+            /*
+            console.log('here');
+            const responseText = await response.text();
+            console.log(responseText);
+            /*
+            const parser = new DOMParser();
+            const htmlDoc = parser.parseFromString(responseText, "text/html");
+            console.log(htmlDoc);
+            */
+        /*
+            const data = eval(
+              responseText.split("setData(")[1].split(", false")[0]
+            );
+            const historicalData = data.reduce((acc, day) => {
+              const date = moment.unix(day[0] / 1000).format("YYYY-MM-DD");
+              acc[date] = day[1];
+              return acc;
+            }, {});
+            saveFile(historicalData);
+            await browser.close();
+          }
+        });
+        */
+        /*
+        await page
+          .waitForSelector("#applyBtn")
+          .then(() => page.click("#applyBtn"));
+          */
+      } else {
+        log(colors.red("Unavailable symbol! 😖\n"));
+        await browser.close();
+      }
+    }
+  });
+})();
