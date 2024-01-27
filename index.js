e5859bef310ea6220abfe4384f16b67bc7039b33
André Ascensão <andremiguelaa@gmail.com>
2024-01-27 02:43:39 +0000

    improvement

--- index.js
+++ index.js
@@ -126,9 +126,6 @@ if (argv.type === "crypto") {
       await page.setUserAgent(
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
       );
-      await page.goto(`https://www.investing.com/`, {
-        waitUntil: "networkidle0",
-      });
       const data = await page.evaluate(
         (cid, initialDate, endDate) => {
           return fetch(
@@ -184,9 +181,6 @@ if (argv.type === "crypto") {
                 await page.setUserAgent(
                   "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
                 );
-                await page.goto(`https://www.investing.com/`, {
-                  waitUntil: "networkidle0",
-                });
                 const data = await page.evaluate(
                   (cid, initialDate, endDate) => {
                     return fetch(
